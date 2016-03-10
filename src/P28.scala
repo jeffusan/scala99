88319f00bf345e3965a9b9a0e99646b23b7a0929
remi thieblin <remi.thieblin@creditkarma.com>
2016-03-10 10:06:20 -0800

    p28 part2

--- src/P28.scala
+++ src/P28.scala
@@ -1,5 +1,7 @@
 object P28 {
 
+  import P10.encode
+
 //  (**) Sorting a list of lists according to length of sublists.
 //    a) We suppose that a list contains elements that are lists themselves. The objective is to sort the elements of the list according to their length. E.g. short lists first, longer lists later, or vice versa.
 //    Example:
@@ -15,8 +17,9 @@ object P28 {
 //  Note that in the above example, the first two lists in the result have length 4 and 1 and both lengths appear just once. The third and fourth lists have length 3 and there are two list of this length. Finally, the last three lists have length 2. This is the most frequent length.
   def main(args: Array[String]) {
 
-  println(lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))))
-//  println(5 /2)
+//  println(lsort_solution(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))))
+
+  println(lsortFreq(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))))
   }
 
   def lsort[A](list: List[List[A]]): List[List[A]] = {
@@ -61,5 +64,19 @@ object P28 {
 
     }
   }
+
+  def lsort_solution[A](list: List[List[A]]): List[List[A]] = {
+    list.sortWith(_.length <= _.length )
+  }
+
+  def lsortFreq[A](list: List[List[A]]): List[List[A]] = {
+
+    list.sortWith((left, right) => list.count(ls => ls.length == left.length) <= list.count(ls => ls.length == right.length))
+  }
+
+//  def lsortFreq_solution[A](ls: List[List[A]]): List[List[A]] = {
+//    val freqs = Map(encode(ls map { _.length } sortWith { _ < _ }) map { _.swap }:_*)
+//    ls sortWith { (e1, e2) => freqs(e1.length) < freqs(e2.length) }
+//  }
 
 }
