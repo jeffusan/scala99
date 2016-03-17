0b0a3a08ba815322fa03cbc72547d036c3b89187
remi thieblin <remi.thieblin@creditkarma.com>
2016-03-17 10:04:07 -0700

    p32

--- src/arithmetic/S99Int.scala
+++ src/arithmetic/S99Int.scala
@@ -26,7 +26,32 @@ object S99Int {
   implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
 
   def main(args: Array[String]) {
-    println(234234239.isPrime)
+//    println(234234239.isPrime)
+
+    println(gcd(36, 63))
+    println(gcd(63, 36))
+  }
+
+  /**
+   * P32 (**) Determine the greatest common divisor of two positive integer numbers.
+   * Use Euclid's algorithm.
+   * The version of the Euclidean algorithm described above (and by Euclid) can take many subtraction steps
+   * to find the GCD when one of the given numbers is much bigger than the other.
+   * A more efficient version of the algorithm shortcuts these steps, instead replacing the larger
+   * of the two numbers by its remainder when divided by the smaller of the two. With this improvement,
+   * the algorithm never requires more steps than five times the number of digits (base 10) of the smaller integer.
+   * This was proven by Gabriel Lamé in 1844, and marks the beginning of computational complexity theory.
+   */
+  def gcd(i: Int, j: Int): Int = {
+
+    if (i > j) gcd(j, i)
+    else if(i == 0) j
+    else {
+      val mod = j % i
+      gcd(i, mod)
+    }
+
+
 
   }
 }
