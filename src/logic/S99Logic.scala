0dd1099b608e7c2bddf1fb19e791377dc64decc3
Remi Thieblin <remi.thieblin@creditkarma.com>
2016-05-27 09:55:20 -0700

    p49

--- src/logic/S99Logic.scala
+++ src/logic/S99Logic.scala
@@ -22,7 +22,11 @@ object S99Logic {
   implicit def bool2S99Logic(a: Boolean): S99Logic = new S99Logic(a)
 
   def main(args: Array[String]) {
-    println(table2(impl))
+//    println(table2(impl))
+    println(gray(1))
+    println(gray(2))
+    println(gray(3))
+    println(gray(4))
 
   }
 
@@ -43,6 +47,24 @@ object S99Logic {
          b <- List(true, false)} {
       printf("%-5s %-5s %-5s\n", a, b, f(a, b))
     }
+  }
+
+  private var memoizedGrays: Map[Int, List[String]] = Map()
+
+  def gray(n: Int): List[String] = {
+
+    memoizedGrays.get(n) match {
+      case Some(g) => g
+      case None =>
+        if(n == 0) List("")
+        else {
+          val lower = gray(n-1)
+          val g = lower.map("0" + _) ::: lower.reverse.map("1" + _)
+          memoizedGrays += (n ->  g)
+          g
+        }
+    }
+
   }
 
 }
