ec622a52d5e3ce07f208936428ff53067dedb5a5
remi.thieblin <remi.thieblin@creditkarma.com>
2017-07-18 17:54:57 -0700

    monoids

---
+++ src/main/scala/cats/monoid/Monoid.scala
@@ -0,0 +1,47 @@
+package cats.monoid
+
+trait Semigroup[A] {
+  def combine(x: A, y: A): A
+}
+
+trait Monoid[A] extends Semigroup[A] {
+  def empty: A
+}
+
+
+object Monoid {
+
+  def associativeLaw[A](x: A, y: A, z: A)
+                       (implicit m: Monoid[A]): Boolean =
+    m.combine(x, m.combine(y, z)) == m.combine(m.combine(x, y), z)
+
+  def identityLaw[A](x: A)
+                    (implicit m: Monoid[A]): Boolean = {
+    (m.combine(x, m.empty) == x) && (m.combine(m.empty, x) == x)
+  }
+
+  val andMonoid = new Monoid[Boolean] {
+    override def empty: Boolean = true
+
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  val orMonoid = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  val addSetMonoid = new Monoid[Set] {
+    override def empty: Set = Set()
+
+    override def combine(x: Set, y: Set): Set = x ++ y
+  }
+
+  val removeSetMonoid = new Monoid[Set] {
+    override def empty: Set = Set()
+
+    override def combine(x: Set, y: Set): Set = x -- y
+  }
+
+}
